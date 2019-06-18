c4192ffc2171748a8950ed787b7e7e46175cc1af
isulley77 <isulley@stuy.edu>
2019-06-18 16:51:36 -0400

    added FileNotFoundException and assign info to variables

--- USACO.java
+++ USACO.java
@@ -3,13 +3,28 @@ import java.util.*;
 
 public class USACO{
 
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
     File f = new File(filename);
     Scanner s = new Scanner(f);
 
     int R, C, E, N;
+    int solution = 0;
+    int[] assign = new int[4];
+
+    for(int i = 0; i < 4; i++){
+      assign[i] = Integer.parseInt(s.next());
+
+    }
+
+    R = assign[0];
+    C = assign[1];
+    E = assign[2];
+    N = assign[3];
 
 
+
+
+    return solution;
   }
 
   //public static int silver(String filename);
