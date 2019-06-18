6c3b6bd3e1a1796949eecb3803d841e5317c295d
isulley77 <isulley@stuy.edu>
2019-06-18 16:43:17 -0400

    began bronze()

--- USACO.java
+++ USACO.java
@@ -2,5 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class USACO{
-  
+
+  public static int bronze(String filename){
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
+
+    int R, C, E, N;
+
+
+  }
+
+  //public static int silver(String filename);
 }
